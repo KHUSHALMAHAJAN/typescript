155be391995c0eb0d57da9aba4767023b5819f2d
khushal mahajan <khushalmahjana303@gmail.com>
2025-08-31 21:14:00 +0530

    add code

--- project/project_1/script.js
+++ project/project_1/script.js
@@ -4,10 +4,13 @@ from === null || from === void 0 ? void 0 : from.addEventListener("submit", func
     val.preventDefault();
 });
 let people = [];
+// let username : string[]
+let i = 0;
 let sumbit = document.querySelector("submit");
 let input = document.querySelectorAll("input");
 let cardconatiner = document.querySelector("#cardconatiner");
-let addconatin;
+let search = document.querySelector("#search");
+let addconatin = "";
 class cratetemplet {
     constructor(username, name, age) {
         this.username = username;
@@ -25,7 +28,17 @@ class cratetemplet {
     }
 }
 from === null || from === void 0 ? void 0 : from.addEventListener("submit", function () {
-    console.log("hu");
-    people[0] = new cratetemplet(input[0].value, input[1].value, Number(input[2].value));
+    // console.log("hu")
+    let username;
+    username = input[0].value;
+    username = new cratetemplet(input[0].value, input[1].value, Number(input[2].value));
+    people.push(username);
+    // console.log(people)
     // people[0].display()
+});
+search === null || search === void 0 ? void 0 : search.addEventListener("input", (e) => {
+    let target = e.target.value;
+    target;
+    let find = people.filter(person => person.username.toLowerCase().startsWith(target.toLowerCase()));
+    console.log(find);
 });
--- project/project_1/script.ts
+++ project/project_1/script.ts
@@ -3,12 +3,15 @@ from?.addEventListener("submit",function(val){
     val.preventDefault()
 })
 let people = []
+// let username : string[]
+let i = 0
 let sumbit = document.querySelector("submit")
 let input = document.querySelectorAll("input")
 let cardconatiner = document.querySelector("#cardconatiner")
-let addconatin : string
+let search = document.querySelector("#search") as HTMLInputElement | null
+let addconatin = ""
 class cratetemplet{
-    constructor(private username :string,private name : string,private age : number){
+    constructor(public username :string,private name : string,private age : number){
         this.display()
     }
     public display(): void{
@@ -22,7 +25,23 @@ class cratetemplet{
     }
 }
 from?.addEventListener("submit",function(){
-    console.log("hu")
-    people[0] = new cratetemplet(input[0].value,input[1].value,Number(input[2].value))
+    // console.log("hu")
+    let username : any
+    username = input[0].value
+    username = new cratetemplet(input[0].value,input[1].value,Number(input[2].value))
+    people.push(username)
+    // console.log(people)
     // people[0].display()
 })
+search?.addEventListener("input", (e) => {
+    let target = (e.target as HTMLInputElement).value;
+    target as string
+
+    let find = people.filter(person => 
+        person.username.toLowerCase().startsWith(target.toLowerCase())
+    );
+
+    console.log(find);
+});
+
+
